451933a17b40bbcba6efe70391aabe421f4556c8
tamarkovaliovi <20222013305@std.nisantasi.edu.tr>
2026-05-25 10:36:51 +0300

    fix: resolve module errors and clean up application structure

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views 
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from blog import views as blog_views 
 from django.conf import settings
 from django.conf.urls.static import static
+
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', blog_views.register, name='register'),
-    path('profile/', blog_views.profile, name='profile'), 
+    path('profile/', blog_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('', include('blog.urls')),
